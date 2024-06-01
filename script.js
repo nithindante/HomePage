02e29bdace041d6a972c518a0fa584d986b9b14f
nithindante <Rajkumar_nithin@yahoo.com>
2024-06-02 01:54:52 +0530

    Made the webpage responsive and made sure it works properly in mobile version, tab and desktop version, added scrolling functionality as well

--- script.js
+++ script.js
@@ -2,17 +2,33 @@
 let link_texts = document.getElementsByClassName("links");
 let links_text_arr = Array.from(link_texts);
 
-
-links_text_arr.forEach((element) => {
-    element.addEventListener("onmouseenter", () => {
-
-        console.log("true")
+for (let i = 0; i < link_texts.length; i++) {
+    link_texts[i].addEventListener("mouseover", () => {
+       ((link_texts[i]).nextElementSibling).classList.toggle("show");
     })
-})
+}
 
 for (let i = 0; i < link_texts.length; i++) {
-    link_texts[i].style.zIndex == "2";
-    link_texts[i].addEventListener("click`", () => {
-        console.log("andi")
+    link_texts[i].addEventListener("mouseout", () => {
+       ((link_texts[i]).nextElementSibling).classList.toggle("show");
     })
-}   
+}
+
+for (let i = 0; i < link_texts.length; i++) {
+    link_texts[i].addEventListener("click", () => {
+        if (link_texts[i].textContent == "Projects") 
+        {
+            let projects = document.getElementsByClassName("projects");
+            for (let index = 0; index < projects.length; index++) {
+                projects[index].scrollIntoView()
+            }
+        }
+        if (link_texts[i].textContent == "Contact me!") 
+        {
+            let contact = document.getElementsByTagName("footer");
+            for (let index = 0; index < contact.length; index++) {
+                contact[index].scrollIntoView()
+            }
+        }
+    })
+}
